218600bf5f89a07745dcc8a6d3a2a711fe93e1e3
ltsRoy <114439860+ltsRoy@users.noreply.github.com>
2024-09-17 12:46:08 +0530

    Create 1.1.2.cpp

---
+++ Striver's A2Z/1.1.2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int x = 10;
+    long long y = 23423423423432;
+    float z = 5.6;
+    
+    cout << x << endl << y << endl << z << endl ;
+    string s;
+    getline(cin, s);
+    cout << s << endl;
+    
+    char ch = 'A';
+    cout << ch;
+    return 0;
+}
