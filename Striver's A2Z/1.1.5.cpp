213a53298062257d76c4992848f3633dce74fff0
ltsRoy <114439860+ltsRoy@users.noreply.github.com>
2024-09-17 13:25:32 +0530

    Create 1.1.5.cpp

---
+++ Striver's A2Z/1.1.5.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int arr [2];
+    cin >> arr[0] >> arr[1];
+    arr[1] += 32;
+    cout << arr[-2] << endl;
+    int arr2 [3][5];
+    string s = "Soumyajit";
+    s[0] = ' A' works but s[0] = "A" wont.
+    int len = s.size();
+    cout << s[len-1];
+    
+}
