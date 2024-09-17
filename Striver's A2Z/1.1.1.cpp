d7e526ae4728494f149813080e87cc939509e3c0
ltsRoy <114439860+ltsRoy@users.noreply.github.com>
2024-09-17 12:36:16 +0530

    Create 1.1.1.cpp

---
+++ Striver's A2Z/1.1.1.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int x,y;
+    cin >> x >> y;
+    std::cout << "Hey Soumyajit!" << std::endl << "\n";
+    cout << "Hey Striver!";
+    return 0;
+}
