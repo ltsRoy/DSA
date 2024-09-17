ce264d06b632cca5f0a7a34cc5a1c49ca5ac1536
ltsRoy <114439860+ltsRoy@users.noreply.github.com>
2024-09-17 13:03:30 +0530

    Create 1.1.4.cpp

---
+++ Striver's A2Z/1.1.4.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int day;
+    cin >> day;
+    switch (day) {
+        case 1:
+        cout << "Monday";
+        break;
+        
+        case 2:
+        cout << "Tuesday";
+        break;
+        
+        cout << "lost";
+        
+        default:
+            cout << "Invalid";
+    }
+    return 0;
+}
