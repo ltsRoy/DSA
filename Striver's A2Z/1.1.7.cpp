edc08fc5fc44854b02ac2358787aeaf2973261e5
ltsRoy <114439860+ltsRoy@users.noreply.github.com>
2024-09-18 21:04:14 +0530

    Create 1.1.7.cpp

---
+++ Striver's A2Z/1.1.7.cpp
@@ -0,0 +1,17 @@
+
+
+// Online C++ compiler to run C++ program online
+#include <iostream>
+using namespace std;
+
+void printName(string name)
+{
+    cout << "Hey " << name;
+}
+
+int main() {
+    string name;
+    cin >> name;
+    printName(name);
+    return 0;
+}
