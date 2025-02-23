a24bf02c4b2336a6cd40f1c4f4deede8faa8413a
ltsRoy <i.am.the.soumyajit.roy@gmail.com>
2025-02-23 21:16:30 +0530

    .

---
+++ Striver's A2Z/Linked List/LengthLL.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+class Node{
+    int data;
+    Node* next;
+
+    Node (int data1, Node* next1){
+        data=data1;
+        next=next1;
+    }
+
+    Node (int data1){
+        data=data1;
+        next=nullptr;
+    }
+}
+
+int main(){
+    vector<int> arr = {1,2,3,4};
+    Node* y = new Node(arr[0]);
+    cout<<y<<"\n";
+    cout<<y->data<<"\n";
+    cout<<y->next<<"\n";
+}
