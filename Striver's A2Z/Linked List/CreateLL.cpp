427ec25daf0e3dd2182f6706f1ddc380050eb184
ltsRoy <i.am.the.soumyajit.roy@gmail.com>
2025-02-23 21:05:46 +0530

    .

---
+++ Striver's A2Z/Linked List/CreateLL.cpp
@@ -0,0 +1,30 @@
+#include <vector>   
+#include <iostream>
+
+using namespace std;
+
+class Node {
+public: 
+    int data;
+    Node* next;
+
+    Node(int data1, Node* next1) {
+        data = data1;
+        next = next1;
+    }
+
+    Node(int data1) {
+        data = data1;
+        next = nullptr;
+    }
+};
+
+int main() {
+    vector<int> arr = {2, 5, 8, 7};
+    Node* y = new Node(arr[0]);
+    cout << y << '\n';
+    cout << y->data << '\n';
+
+    delete y; 
+    return 0;
+}
